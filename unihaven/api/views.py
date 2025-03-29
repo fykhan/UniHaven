79058990e6dc70cfc85f0d0c50f87850472d1a11
Xiao Heng <130291679+Satire0v0@users.noreply.github.com>
2025-03-29 15:46:33 +0800

    Update views.py

--- unihaven/api/views.py
+++ unihaven/api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class HealthCheck(APIView):
+    def get(self, request):
+        return Response({'status': 'OK'}, status=status.HTTP_200_OK)
