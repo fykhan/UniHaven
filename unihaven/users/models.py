b8c6944ca39e29f2f52970a655c7ab24793aef55
A zzz <136796251+Anderson-ops-oss@users.noreply.github.com>
2025-03-29 08:41:56 +0800

    Update models.py

--- unihaven/users/models.py
+++ unihaven/users/models.py
@@ -3,13 +3,6 @@ from djang0.contrib.auth.models import AbstractUser, Group, Permission
 
 # Create User class
 class User(AbstractUser):
-  class Role(models.TextChoices):
-    STUDENT = 'ST', 'Student'
-    STAFF = 'SF', 'Staff'
-    ADMIN = 'AD', 'Admin'
-    
-  role = models.CharField(max_length=2, choices=Role.choices, default=Role.STUDENT)
-  hku_id = models.CharField(max_length=10, unique=True, null=True, blank=True)
   phone_number = models.CharField(max_length=15, blank=True)
 
   def __str__(self):
