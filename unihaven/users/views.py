3a8ba7804d9f933623b9131603839d4e611a192b
A zzz <136796251+Anderson-ops-oss@users.noreply.github.com>
2025-04-01 09:06:39 +0800

    Update views.py

--- unihaven/users/views.py
+++ unihaven/users/views.py
@@ -8,18 +8,28 @@ from .models import User
 @csrf_exempt
 def login_view(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
-        username = data.get('username')
-        password = data.get('password')
+        # data = json.loads(request.body)
+        # username = data.get('username')
+        # password = data.get('password')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         
         if user is not None:
             login(request, user)
-            return JsonResponse({
-                'message': 'Login successful', 
-                'is_student': user.is_student, 
-                'is_cedars_staff': user.is_cedars_staff
-            })
+            # return JsonResponse({
+            #     'message': 'Login successful', 
+            #     'is_student': user.is_student, 
+            #     'is_cedars_staff': user.is_cedars_staff
+            # })
+            if user.is_student:
+                return redirect('student_selection') 
+            elif user.is_property_owner:
+                return JsonResponse({'redirect_url': 'http://127.0.0.1:8000/api/accommodations/'})
+            elif user.is_cedars_staff or user.is_admin:
+                return JsonResponse({'redirect_url': 'http://127.0.0.1:8000/admin/'})
+            else:
+                return JsonResponse({'redirect_url': 'http://127.0.0.1:8000/'})
         else:
             return JsonResponse({'message': 'Invalid credentials'}, status=400)
     elif request.method == 'GET':
@@ -29,3 +39,8 @@ def logout_view(request):
     if request.method == 'POST':
         logout(request)
         return JsonResponse({'message': 'Logout successful'})
+    elif request.method == 'GET':
+        return redirect('http://127.0.0.1:8000/')
+
+def student_selection_view(request):
+    return render(request, 'student_selection.html') 
