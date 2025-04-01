36586830880e0f9e40dfaa222a4d000333bd8ec5
A zzz <136796251+Anderson-ops-oss@users.noreply.github.com>
2025-04-01 09:05:01 +0800

    Update urls.py

--- unihaven/users/urls.py
+++ unihaven/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import student_selection_view
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('student_selection/', student_selection_view, name='student_selection'), 
 ]
